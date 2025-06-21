3e9f79919725f0c4d830eddfc20a3b845df60c6e
jpaulgb <jepgb@hotmail.com>
2025-06-21 13:43:42 -0500

    seven commit

--- prueba.js
+++ prueba.js
@@ -2,6 +2,7 @@ console.log("repasando github")
 let numeroUno = 10
 let numeroDos = 20
 
+function comparacion(){
 if(numeroUno < numeroDos){
     console.log("condición verdadera")
     for(let i = 0; i<8 ; i++){
@@ -10,17 +11,17 @@ if(numeroUno < numeroDos){
 }else{
     console.log("condición falsas")
 }
-
+}
 
 for(let i = 0; i<8 ; i++){
     console.log(i)
 }
 
-//commit numero 5
+//commit numero 7
 
 /* COMANDOS 
 
-git add.
+git add .
 git commit -m "second commit"
 git push origin main
 */
