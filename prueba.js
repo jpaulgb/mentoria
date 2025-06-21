fbe34047c86653169aea4764cb452e29c8cf9fbd
jpaulgb <jepgb@hotmail.com>
2025-06-21 13:41:07 -0500

    six commit

--- prueba.js
+++ prueba.js
@@ -4,9 +4,13 @@ let numeroDos = 20
 
 if(numeroUno < numeroDos){
     console.log("condición verdadera")
+    for(let i = 0; i<8 ; i++){
+    console.log(i)
+}
 }else{
     console.log("condición falsas")
 }
+
 
 for(let i = 0; i<8 ; i++){
     console.log(i)
