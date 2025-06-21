d26ba6ab9f39557bb29c092393775ee138000ac6
jpaulgb <jepgb@hotmail.com>
2025-06-21 13:24:35 -0500

    second commit

--- prueba.js
+++ prueba.js
@@ -1,1 +1,15 @@
 console.log("repasando github")
+let numeroUno = 10
+let numeroDos = 20
+
+if(numeroUno < numeroDos){
+    console.log("condición verdadera")
+}else{
+    console.log("condición falsas")
+}
+
+/* COMANDOS 
+
+git add.
+
+*/
